bf54f365c8d7fbe00d641a671500e14049fea56e
David Eliason <davethemaker@gamil.com>
2016-11-23 18:28:57 -0800

    add form to view

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-	res.render('todo.ejs');
+	var list = [];
+	res.render('todo.ejs',{list:list});
 });
 
 app.listen(8080);
