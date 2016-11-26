2a8bca698f8abd9c4930e41a120e2bcf0020f45c
David Eliason <davethemaker@gamil.com>
2016-11-25 16:14:16 -0800

    add form for adding task, grab that value and send push it into tasks array, send array to ejs view

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 app.use(morgan('combined'))
-.use(express.static(__dirname + '/public'));
+.use(express.static(__dirname + '/public'))
+.use(bodyParser());
+
+app.get('/',function(req,res){
+  var html = '<form action="/" method="post">' +
+	               'Enter your name:' +
+	               '<input type="text" name="task" placeholder=" feed the fish.." />' +
+	               '<br>' +
+	               '<button style="background-color: blue" type="submit">Confirm</button>' +
+              '</form>';
+               
+  res.send(html);
+});
+
+app.post('/', function(req, res){
+  // var userName = req.body.userName;
+  // var html = 'Hello: ' + userName + '.<br>' +
+  //            '<a href="/">Try again.</a>';
+  var tasks = [];
+  var task = req.body.task;
+  for(var key in task){
+  	if(task.hasOwnProperty(key)){
+  		tasks.push(task[key]);
+  	}
+  } // add the new task to the array
+  console.log(tasks.length + " is the task list length");
+  res.render('todo.ejs',{ task :req.body.task  });
+});
 
 app.listen(8080);
